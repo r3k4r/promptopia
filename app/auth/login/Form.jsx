53b93ca82f16f11394facb6460e406ea540755b8
r3k4r <rekarjamal33@gmail.com>
2024-09-02 01:00:45 +0300

    k

--- app/auth/login/Form.jsx
+++ app/auth/login/Form.jsx
@@ -1,13 +1,16 @@
+'use client' 
 
-import Password from "@/app/components/Password";
+import { EyeSlashIcon, EyeIcon } from "@heroicons/react/24/solid";
+import { useState } from "react"
 import Social from "../../components/Social";
 import Link from "next/link";
 import {signIn} from '@/app/lib/action'
-const Form = async () => {
-const login=await signIn();
+const Form = () => {
+    const [passwordShown, setShowPassword] = useState(false);
+
 
   return (
-    <form action={login} className="flex flex-col space-y-5 mt-5 w-[370px] ">
+    <form action={"#"} className="flex flex-col space-y-5 mt-5 w-[370px] ">
         <div className="flex flex-col space-y-1">
             <label htmlFor="email" className="text-sm font-semibold">Email</label>
             <input type="email" id="email" className="p-2 border border-gray-300 rounded-md" />
@@ -15,14 +18,14 @@ const login=await signIn();
 
         <div className="flex flex-col space-y-1 w-full">
         <label htmlFor="password" className="text-sm font-semibold">Password</label>
-        <input type="password" id="password" className="peer p-2 border border-gray-300 rounded-md " />
-        {/* <i className=" text-gray-400 peer-focus:text-gray-900" onClick={() => setShowPassword(!passwordShown)}>
+        <input type={passwordShown ? "text" : "password"} id="password" className="peer p-2 border border-gray-300 rounded-md " />
+        <i className="absolute left-[798px] bottom-[294px] text-gray-400 peer-focus:text-gray-900" onClick={() => setShowPassword(!passwordShown)}>
         {passwordShown ? (
             <EyeIcon className="h-5 w-5" />
         ) : (
             <EyeSlashIcon className="h-5 w-5" />
         )}
-        </i> */}
+        </i>
     </div>
 
         <button type="submit" className="bg-primary-orange hover:bg-orange-600 text-white p-3 rounded-md font-bold">Login</button>
