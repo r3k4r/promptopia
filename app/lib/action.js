4395b7c2d786fffcd17985760be398d95f91783f
r3k4r <rekarjamal33@gmail.com>
2024-09-01 22:47:24 +0300

    l

--- app/lib/action.js
+++ app/lib/action.js
@@ -4,6 +4,8 @@
 import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
 import bcrypt from 'bcrypt'
+import { ResultCode } from "@/app/lib/action"; 
+
 
 const prisma = new PrismaClient();
 export async function signIn(FormData){
@@ -23,6 +25,19 @@ export async function signUp(prevstate, formData){
        const password= formData.get("password")
        const name = formData.get("name")
        const email = formData.get("email")
+
+       const existingUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+
+    if(existingUser){
+        return{
+            type:'error',
+            resultCode: 
+        }
+    }
 
        const hashedPassword =await bcrypt.hash(password, 12)
 
