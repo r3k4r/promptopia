8ab438206aa0617f8de0ce6a3f433345d99cbf7a
r3k4r <rekarjamal33@gmail.com>
2024-09-02 19:19:06 +0300

    k

--- auth.config.js
+++ auth.config.js
@@ -1,7 +1,45 @@
-import GitHub from "next-auth/providers/github"
-import  Google  from 'next-auth/providers/google';
- 
+// import GitHub from "next-auth/providers/github"
+// import  Google  from 'next-auth/providers/google';
+import Credentials from "next-auth/providers/credentials" 
+import { getUserByEmail } from "./data/user";
+import bcrypt from 'bcrypt'
 
 export default {
-  providers: [GitHub, Google],
+  providers: [
+    Credentials({
+      async authorize(credentials){
+        const { email, password } = credentials;
+        const validate = password >= 8 ;
+        
+        if(validate){
+          const user = await getUserByEmail(email);
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if(!user || !user.password) return null;
+
+          if(passwordMatch){
+            return user;
+          }
+        }
+        
+      }
+      
+    })
+  ],
+}
+
+function validateEmail(email) {
+  // Email validation logic here
+  // You can use a regular expression or any other method to validate the email format
+  // Example regular expression for email validation:
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  // Password validation logic here
+  // You can use a regular expression or any other method to validate the password format
+  // Example regular expression for password validation:
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
 } 
---
+++ data/user/index.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getUserByEmail(email) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+    return user;
+}
